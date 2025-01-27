bf446311b077873a8deb49dcafd39f52adf9e966
Lenya Nab <nabiev.lenya@gmail.com>
2025-01-27 10:43:44 +0400

    Add launch wrapper

--- launch.go
+++ launch.go
@@ -1,13 +1,16 @@
 package main
 
 // twosum "github.com/et0/leetcode/Easy/0001_two_sum"
-import (
-	l0021 "github.com/et0/leetcode/Easy/0021_Merge_Two_Sorted_Lists"
-)
+// l0021 "github.com/et0/leetcode/Easy/0021_Merge_Two_Sorted_Lists"
 
 func main() {
-	l0021.Wrapper(&l0021.ListNode{Val: 1, Next: &l0021.ListNode{Val: 2, Next: &l0021.ListNode{Val: 4, Next: nil}}}, &l0021.ListNode{Val: 1, Next: &l0021.ListNode{Val: 3, Next: &l0021.ListNode{Val: 4, Next: nil}}})
-	// fmt.Println(validparentheses.Wrapper("()({[]})[]")) 0020
+
+	// searchinsertposition.Wrapper([]int{1, 3, 5}, 5) // 0035
+	// l0028.Wrapper("c", "ccc") // 0028
+	// removeelement.Wrapper([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2) // 0027
+	// fmt.Println(l0026.Wrapper("()({[]})[]")) // 0026
+	// l0021.Wrapper(&l0021.ListNode{Val: 1, Next: &l0021.ListNode{Val: 2, Next: &l0021.ListNode{Val: 4, Next: nil}}}, &l0021.ListNode{Val: 1, Next: &l0021.ListNode{Val: 3, Next: &l0021.ListNode{Val: 4, Next: nil}}}) // 0021
+	// fmt.Println(validparentheses.Wrapper("()({[]})[]")) // 0020
 	// fmt.Println(longestcommonprefix.Wrapper([]string{"a"})) // 0014
 	// fmt.Println(romantointeger.Wrapper("MCMXCIV")) // 0013
 	// fmt.Println(palindromenumber.Wrapper(6466)) // 0009
